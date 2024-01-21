2699e45a4e451d95144f0e492d9f68858b5ff8fd
Nadezhda Komarova <naduha.9612@gmail.com>
2024-01-22 00:17:16 +0300

    feat: add solution task 01

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
+const file = path.dirname(__filename) + `\\text.txt`;
+console.log(file);
+const stream = fs.createReadStream(file, "utf-8");
+
+let data = "";
+
+stream.on("data", (chunk) => console.log(data += chunk));
