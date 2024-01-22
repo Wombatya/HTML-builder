95a5be8a9fa37614b1d2def5767334a65da2d8e2
Nadezhda K <77297047+Wombatya@users.noreply.github.com>
2024-01-22 12:46:10 +0300

    fix: change path

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const file = path.dirname(__filename) + `\\text.txt`;
+const file = path.dirname.join(__filename, "text.txt");
 const stream = fs.createReadStream(file, "utf-8");
 
 let data = "";
