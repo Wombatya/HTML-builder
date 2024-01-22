d70e25dcf2df353ff2542f7d24540236ad5b67fc
Nadezhda K <77297047+Wombatya@users.noreply.github.com>
2024-01-22 11:19:13 +0300

    fix: delete unnecessary line

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const path = require("path");
 
 const file = path.dirname(__filename) + `\\text.txt`;
-console.log(file);
 const stream = fs.createReadStream(file, "utf-8");
 
 let data = "";
